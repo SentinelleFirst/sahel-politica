83050a9c3e1e650f7467a10b4cbd9ab16a53e2b7
SentinelleFirst <sentinelle.dev@gmail.com>
2024-07-26 20:23:45 +0000

    Setting langage choice

--- indexJs.js
+++ indexJs.js
@@ -400,3 +400,25 @@ document.getElementById('contactButton').addEventListener('click', async () => {
     console.error('Exception while sending email:', error);
   }
 });  
+
+//Langue manager
+
+const languageButton = document.getElementById('languageButton');
+    const languageMenu = document.getElementById('languageMenu');
+
+    languageButton.addEventListener('click', () => {
+      languageMenu.style.display = languageMenu.style.display === 'block' ? 'none' : 'block';
+    });
+
+    document.querySelectorAll('#languageMenu li').forEach(item => {
+      item.addEventListener('click', () => {
+        const lang = item.getAttribute('data-lang');
+        document.querySelectorAll('.lang').forEach(el => {
+          el.style.display = 'none';
+        });
+        document.querySelectorAll(`.lang.${lang}`).forEach(el => {
+          el.style.display = 'block';
+        });
+        languageMenu.style.display = 'none';
+      });
+    });
