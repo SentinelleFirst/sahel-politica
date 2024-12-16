af324c67398c32d4284b77cfbf5edad55f29103b
lincorrect21 <sawadogo.marck@gmail.com>
2024-12-16 01:16:32 +0000

    new file:   API keys.txt 	modified:   about-us.html 	modified:   js/indexJs.js 	new file:   js/subscription.js

--- js/indexJs.js
+++ js/indexJs.js
@@ -2,21 +2,50 @@
 //Langue manager
 
 const languageButton = document.getElementById('languageButton');
-    const languageMenu = document.getElementById('languageMenu');
+const languageMenu = document.getElementById('languageMenu');
 
-    languageButton.addEventListener('click', () => {
-      languageMenu.style.display = languageMenu.style.display === 'block' ? 'none' : 'block';
-    });
+languageButton.addEventListener('click', () => {
+  languageMenu.style.display = languageMenu.style.display === 'block' ? 'none' : 'block';
+});
 
-    document.querySelectorAll('#languageMenu li').forEach(item => {
-      item.addEventListener('click', () => {
-        const lang = item.getAttribute('data-lang');
-        document.querySelectorAll('.lang').forEach(el => {
-          el.style.display = 'none';
-        });
-        document.querySelectorAll(`.lang.${lang}`).forEach(el => {
-          el.style.display = 'block';
-        });
-        languageMenu.style.display = 'none';
-      });
+document.querySelectorAll('#languageMenu li').forEach(item => {
+  item.addEventListener('click', () => {
+    const lang = item.getAttribute('data-lang');
+    document.querySelectorAll('.lang').forEach(el => {
+      el.style.display = 'none';
     });
+    document.querySelectorAll(`.lang.${lang}`).forEach(el => {
+      el.style.display = 'block';
+    });
+    languageMenu.style.display = 'none';
+  });
+});
+
+const form = document.getElementById('newsletter-fil');
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = {
+    email: formData.get('email'),
+    attributes: {
+      FIRSTNAME: formData.get('firstname'),
+    },
+  };
+  try {
+    const response = await fetch('https://api.brevo.com/v3/contacts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'api-key': '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',
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    alert('Subscription successful!');
+  } catch (error) {
+    console.error('Error:', error);
+    alert('There was an error with your subscription.');
+  }
+});
