ca4aae39518b663574d244d6860640072359d042
SentinelleFirst <sentinelle.dev@gmail.com>
2024-12-22 00:41:22 +0000

    Finish translation setting

--- translations/publications-content.js
+++ translations/publications-content.js
@@ -1,3 +1,27 @@
-window.translationsEN = {};
+window.translationsEN = {
+    "publications-hero-title": "PUBLICATIONS",
+    "publications-hero-subtitle": "Get informed, get inspired and stay ahead of the trends with our publications.",
+    "publications-hero-text": "At Sahel Politica, we help you navigate the complexities of the Sahel by offering resilience audits, prarticleion plans, crisis response and expansion strategies.",
+    "publications-socials-title": "Find us on social media",
+    "publications-media-code-title": "Making sure our social media channels meet your expectations",
+    "publications-media-code-1-title": "Social media code of conduct",
+    "publications-media-code-1-text": "Digital interaction offers significant benefits, but it also involves certain risks and responsibilities. To address this, we have established a code of conduct to ensure that the actions of Sahel Politica employees remain safe, positive, and reflective of the true essence of the Sahel Politica brand.",
+    "publications-media-code-1-link": `See our social media code of conduct<img src="images/long-right-arrow.png">`,
+    "publications-media-code-2-title": "Social media house rules",
+    "publications-media-code-2-text": "The house rules set clear expectations for behavior when engaging with us on social media. They aim to maintain a positive experience across our channels and foster communities that are constructive, supportive, and enjoyable for everyone.",
+    "publications-media-code-2-link": `See our social media code of conduct<img src="images/long-right-arrow.png">`,
+};
 
-window.translationsFR = {};
+window.translationsFR = {
+    "publications-hero-title": "PUBLICATIONS",
+    "publications-hero-subtitle": "Informez-vous, inspirez-vous et restez à la pointe des tendances grâce à nos publications.",
+    "publications-hero-text": "A Sahel Politica, nous vous aidons à naviguer dans les complexités du Sahel en vous proposant des audits de résilience, des plans de préparation, des réponses aux crises et des stratégies d'expansion.",
+    "publications-socials-title": "Retrouvez-nous sur les médias sociaux",
+    "publications-media-code-title": "S'assurer que nos canaux de médias sociaux répondent à vos attentes",
+    "publications-media-code-1-title": "Code de conduite pour les médias sociaux",
+    "publications-media-code-1-text": "L'interaction numérique offre des avantages considérables, mais elle comporte également certains risques et responsabilités. Pour y remédier, nous avons établi un code de conduite afin de garantir que les actions des employés de Sahel Politica restent sûres, positives et reflètent l'essence même de la marque Sahel Politica.",
+    "publications-media-code-1-link": `Voir notre code de conduite pour les médias sociaux<img src="images/long-right-arrow.png">`,
+    "publications-media-code-2-title": "Règles de conduite dans les médias sociaux",
+    "publications-media-code-2-text": "Le règlement intérieur définit des attentes claires en matière de comportement lorsque l'on s'engage avec nous sur les médias sociaux. Elles visent à maintenir une expérience positive sur l'ensemble de nos canaux et à favoriser des communautés constructives, solidaires et agréables pour tous.",
+    "publications-media-code-2-link": `Voir notre code de conduite pour les médias sociaux<img src="images/long-right-arrow.png">`,
+};
